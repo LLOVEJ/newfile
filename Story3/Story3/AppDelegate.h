e6f7ad1037a3edb7f761a95da56302e133cd5a07
lyl <957270185@qq.com>
2016-03-10 14:19:04 +0800

    first commit By LYL

---
+++ Story3/Story3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Story3
+//
+//  Created by sd on 16/3/9.
+//  Copyright © 2016年 sd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Story3/Story3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Story3
+//
+//  Created by sd on 16/3/9.
+//  Copyright © 2016年 sd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
